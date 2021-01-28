b374f5a2a954187d92d93f99fea5d99c35548dae
Dmytro Kuchura <kuchura.d@gmail.com>
2021-01-28 20:14:27 +0200

    fixes

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [SiteController::class, 'index'])->name('home');
+
+Route::get('/admin/{uri?}', function () {
+    return view('react');
+})->where('uri', '(.*)');
